9bc739b0b9a359cca8cf6c50f6455349e6377665
Prayag Srivastava <prayag@outlook.in>
2022-04-16 21:20:12 +0530

    test

--- static/js/layout.js
+++
@@ -1,5 +0,0 @@
-function headerSearchForm(event) {
-    event.preventDefault();
-    document.querySelector("#headerSearchFormBtn").blur();
-    return false;
-}
--- store/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.conf import settings
-
-def homepage(request):
-    print(settings.FAVICON_URL)
-    return render(
-        request,
-        'store/homepage.html'
-    )
